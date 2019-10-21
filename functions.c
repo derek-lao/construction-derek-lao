e30494fdc163bf86de97f2175a215b999861cdcb
Derek Lao <dlao00@stuy.edu>
2019-10-21 18:21:30 -0400

    finished bulk of main testing, still have to change some things for printOut, based on testing

--- functions.c
+++ functions.c
@@ -18,14 +18,15 @@ struct food returnStruct()
 //Write a function that prints out variables of your structs type in a reasonable way.
 int printOut(struct food theFood)
 {
-  char answer[1000] = "the name is \"";
+  char answer[1000] = "The name is \"";
   char priceString[100];
   sprintf(priceString, "%f", theFood.price);
   int lengthOfName = strlen(theFood.name);
   int lengthOfPrice = strlen(priceString);
   strncat(answer, theFood.name, lengthOfName);
-  strncat(answer, "and the price is ", strlen("and the price is "));
+  strncat(answer, "\" and the price is ", strlen("\" and the price is "));
   strncat(answer, priceString, strlen(priceString));
+  strncat(answer, "\n", strlen("\n"));
   printf("%s", answer);
   return 0;
 }
--- main.c
+++ main.c
@@ -7,13 +7,21 @@
 //Write a main function that tests all these things.
 int main()
 {
+  printf("going to initialize \"struct food pig\", with \"pig\" as name and 1.0 as price\n");
   struct food pig;
   strncat(pig.name, "pig", strlen("pig"));
   pig.price = 1.0;
+  printf("about to print out pig struct\n");
   printOut(pig);
-  printf("The name is %s and the price is %f", pig.name, pig.price);
+  printf("finished printing out pig\n\n");
+  printf("going to initialize \"struct food randomCandy\", with \"candy\" as name and a random price\n");
   struct food randomCandy = returnStruct();
-  return 0;
-  printf("beep boop");
+  printOut(randomCandy);
+  printf("finished printing out randomCandy\n\n");
+  printf("going to modify pig struct to have \"beef\" as name and 2.0 as price\n");
+  modify(pig, &pig.name, &pig.price, "beef", 2.0);
+  printf("finished modifying pig\n");
+  printOut(pig);
+  printf("finished printing out the new pig\n");
   return 0;
 }
